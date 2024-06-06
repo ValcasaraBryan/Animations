e2faed1450ddb10c0bf88a3885bb2504577d338f
Valcasara Bryan <31510683+ValcasaraBryan@users.noreply.github.com>
2024-06-06 15:35:20 +0200

    Add multiples animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,21 +8,98 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation}
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+    }
+    /*
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation(.bouncy) {
+                            dragAmount = .zero
+                        }
+                        
+                    }
+            )
+            //.animation(.bouncy, value: dragAmount)
+    }
+    */
+    
+    /*
+    @State private var enabled = false
+    var body: some View {
+        Button("Tap Me") {
+            enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
+        .foregroundColor(.white)
+        .animation(nil, value: enabled)
+        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+        .animation(.spring(duration: 1, bounce: 0.9), value: enabled)
+
+        
+    }
+    */
+    /*@State private var animationAmount = 1.0
 
     var body: some View {
         Button("Tap Me") {
             // do nothing
-            animationAmount += 1
+            //animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
+        .overlay {
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount)
+        }
+        .onAppear {
+            animationAmount = 2
+        }
+        //.scaleEffect(animationAmount)
+        //.blur(radius: (animationAmount - 1) * 3)
+        //.animation(.default, value: animationAmount)
     }
+     */
 }
 
 #Preview {
